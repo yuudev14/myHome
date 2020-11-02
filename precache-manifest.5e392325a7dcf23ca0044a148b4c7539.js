d8d3db97f6da81427e9224cd80ec90e380671b80
YuTakaki <yu.takaki2000@gmail.com>
2020-11-02 13:04:01 +0900

    Updates

---
+++ precache-manifest.5e392325a7dcf23ca0044a148b4c7539.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afdc50feb04d260f198e1006b0010204",
+    "url": "/myHome/index.html"
+  },
+  {
+    "revision": "80d952f63ce16e5cb789",
+    "url": "/myHome/static/css/main.bb29adcb.chunk.css"
+  },
+  {
+    "revision": "2fafdd04877a94de26ef",
+    "url": "/myHome/static/js/2.cdffa2da.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/myHome/static/js/2.cdffa2da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80d952f63ce16e5cb789",
+    "url": "/myHome/static/js/main.93feb5b3.chunk.js"
+  },
+  {
+    "revision": "20b25f8d1a85b1f5e839",
+    "url": "/myHome/static/js/runtime-main.01118198.js"
+  },
+  {
+    "revision": "d4c97fa142607766becc232782b081fe",
+    "url": "/myHome/static/media/city.d4c97fa1.jpg"
+  }
+]);
